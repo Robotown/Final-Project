2d2c092217d781399bacfdcf819cff391bda23d8
Brian Moore <bmoore6@ucmerced.edu>
2018-04-28 18:20:48 -0700

    Detection and exploration almost working

--- src/finalpkg/src/explore.cpp
+++ src/finalpkg/src/explore.cpp
@@ -8,6 +8,7 @@ int main(int argc, char **argv)
 {
 
     ros::init(argc, argv, "explore");
+    ros::NodeHandle nh;
 
     actionlib::SimpleActionClient<frontier_exploration::ExploreTaskAction> ac("explore_server", true);
 
@@ -19,24 +20,9 @@ int main(int argc, char **argv)
 
     geometry_msgs::PolygonStamped boundary;
 
-    geometry_msgs::Point32 p1, p2, p3, p4, p5;
-
-    p1.x =  10.0; p1.y =  10.0; p1.z = 0.0;
-    p2.x =  10.0; p2.y = -10.0; p2.z = 0.0;
-    p3.x = -10.0; p3.y = -10.0; p3.z = 0.0;
-    p4.x = -10.0; p4.y =  10.0; p4.z = 0.0;
-    p5.x =  10.0; p5.y =  10.0; p5.z = 0.0;
-
-    boundary.header.seq = 0;
-    boundary.header.stamp = ros::Time::now();
     boundary.header.frame_id = "map";
-
-    boundary.polygon.points.push_back(p1);
-    boundary.polygon.points.push_back(p2);
-    boundary.polygon.points.push_back(p3);
-    boundary.polygon.points.push_back(p4);
-    boundary.polygon.points.push_back(p5);
-
+    boundary.header.stamp = ros::Time::now();
+        
     goal.explore_center.header.frame_id = "map";
     goal.explore_center.point.x = 0.0;
     goal.explore_center.point.y = 0.0;
@@ -45,5 +31,4 @@ int main(int argc, char **argv)
     ac.sendGoal(goal);
 
     return 0;
-
 }
