a97878689d8ecb14583ad1f59b77d8a229a0d469
Jadon Hansell <Alcamore@gmail.com>
2018-04-30 19:21:04 -0700

    Change tag_detect to work with tf2

--- src/finalpkg/src/tag_detect.cpp
+++ src/finalpkg/src/tag_detect.cpp
@@ -7,79 +7,54 @@
 #include <cmath>
 #include <tf2/LinearMath/Quaternion.h>
 #include <tf2/impl/utils.h>
+#include <tf2_ros/transform_listener.h>
 
-geometry_msgs::Pose robot_pose;
 std::vector<std::string> found_tags;
 
-double fix_angle(double angle)
+tf2_ros::Buffer buffer;
+namespace tf2
 {
-    while (angle > 2*M_PI)
-        angle -= 2*M_PI;
-    while (angle < 0)
-        angle += 2*M_PI;
-
-    return angle;
-}
-
-geometry_msgs::Pose combine_pose_with_image(const geometry_msgs::Pose &pose,
-                                            const logical_camera_plugin::logicalImage &image)
-{
-    geometry_msgs::Pose final_pose;
-
-    tf2::Quaternion image_orientation = tf2::Quaternion(image.pose_rot_x,
-                                                        image.pose_rot_y,
-                                                        image.pose_rot_z,
-                                                        image.pose_rot_w);
-    tf2::Quaternion pose_orientation = tf2::impl::toQuaternion(pose.orientation);
-    
-    double yaw = fix_angle(tf2::impl::getYaw(pose_orientation) +
-                           tf2::impl::getYaw(image_orientation));
-
-    if (yaw >= 0 && yaw > M_PI/2) {
-        final_pose.position.x = pose.position.x - image.pose_pos_x;
-        final_pose.position.y = pose.position.y - image.pose_pos_y;
+    inline
+    void convert(const geometry_msgs::TransformStamped& transform, geometry_msgs::PoseStamped& pose)
+    {
+        pose.pose.orientation = transform.transform.rotation;
+        pose.pose.position.x = transform.transform.translation.x;
+        pose.pose.position.y = transform.transform.translation.y;
+        pose.pose.position.z = transform.transform.translation.z;
+        pose.header = transform.header;
     }
-    else if (yaw >= M_PI/2 && yaw < M_PI) {
-        final_pose.position.x = pose.position.x + image.pose_pos_x;
-        final_pose.position.y = pose.position.y - image.pose_pos_y;
-    }
-    else if (yaw >= M_PI && yaw < 3*M_PI/2) {
-        final_pose.position.x = pose.position.x + image.pose_pos_x;
-        final_pose.position.y = pose.position.y + image.pose_pos_y;
-    }
-    else {
-        final_pose.position.x = pose.position.x - image.pose_pos_x;
-        final_pose.position.y = pose.position.y + image.pose_pos_y;
-    }
-
-    final_pose.position.z = 0.0;
-
-
-    tf2::Quaternion temp_orientation;
-    temp_orientation.setRPY(0.0, 0.0, yaw);
-    tf2::convert(temp_orientation, final_pose.orientation);
-
-    return final_pose;
 }
 
 void camera_callback(const logical_camera_plugin::logicalImage &image)
 {
     if (std::find(found_tags.begin(), found_tags.end(), image.modelName) == found_tags.end()) {
         found_tags.push_back(image.modelName);
+        
+        tf2::Vector3 image_position = tf2::Vector3(image.pose_pos_x,
+                                                   image.pose_pos_y,
+                                                   image.pose_pos_z);
+        
+        tf2::Quaternion image_orientation = tf2::Quaternion(image.pose_rot_x,
+                                                            image.pose_rot_y,
+                                                            image.pose_rot_z,
+                                                            image.pose_rot_w);
 
-        geometry_msgs::Pose tag_pose = combine_pose_with_image(robot_pose, image);
-
+        geometry_msgs::TransformStamped image_transform;
+        tf2::convert( tf2::Transform(image_orientation, image_position), image_transform.transform );
+        image_transform.header.stamp = ros::Time::now();
+        image_transform.header.frame_id = "base_link";
+        image_transform.child_frame_id = image.modelName;
+        
+        geometry_msgs::PoseStamped tag_pose;
+        tf2::convert( buffer.transform(image_transform, "map"), tag_pose );
+        
+        
         ROS_INFO_STREAM("Found tag: " << image.modelName << '\n' << tag_pose);
 
-        ROS_INFO_STREAM("\n-----DEBUG-----" << "\n" <<
-                        "ROBOT_POSE:\n" << robot_pose << "\n" <<
-                        "TAG_POSE:\n" << image);
+        ROS_DEBUG_STREAM("\n-----DEBUG-----" << "\n" <<
+                         "TAG_POSE:\n" << image);
     }
-}
 
-void pose_callback(const geometry_msgs::PoseWithCovarianceStamped &pose)
-{
-    robot_pose = pose.pose.pose;
 }
 
 int main(int argc, char* argv[])
@@ -87,8 +62,9 @@ int main(int argc, char* argv[])
     ros::init(argc, argv, "tag_detect");
     ros::NodeHandle nh;
 
-    ros::Subscriber subPose = nh.subscribe("/amcl_pose", 1000, &pose_callback);
     ros::Subscriber subCamera = nh.subscribe("/objectsDetected", 1000, &camera_callback);
+
+    tf2_ros::TransformListener tf_listener(buffer, nh);
 
     ros::spin();
 
